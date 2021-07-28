c34d0c921583c720727e346411bbf7e9f54c91bb
oibanez1 <oibanezdaniel1@gmail.com>
2021-07-28 05:45:26 -0400

    init, added override annotation

--- src/main/java/com/example/dao/UserDAO.java
+++ src/main/java/com/example/dao/UserDAO.java
@@ -4,19 +4,24 @@ import java.util.List;
 import com.example.model.User;
 
 public class UserDAO implements DAO<User> {
+    @Override
     public User get(int id) {
-
+        return null;
     }
+    @Override
     public List<User> getAll() {
-
+        return null;
     }
+    @Override
     public void add(User newUser) {
 
     }
+    @Override
     public boolean delete(int id) {
-
+        return false;
     }
+    @Override
     public boolean update(User user) {
-
+        return false;
     }
 }
