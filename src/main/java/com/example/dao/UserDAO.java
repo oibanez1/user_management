bcf53524e8929d2f9457f3fff6cbc81279c3e0bf
oibanez1 <oibanezdaniel1@gmail.com>
2021-07-28 05:35:00 -0400

    concrete implementation of dao interface init

---
+++ src/main/java/com/example/dao/UserDAO.java
@@ -0,0 +1,22 @@
+package com.example.dao;
+
+import java.util.List;
+import com.example.model.User;
+
+public class UserDAO implements DAO<User> {
+    public User get(int id) {
+
+    }
+    public List<User> getAll() {
+
+    }
+    public void add(User newUser) {
+
+    }
+    public boolean delete(int id) {
+
+    }
+    public boolean update(User user) {
+
+    }
+}
