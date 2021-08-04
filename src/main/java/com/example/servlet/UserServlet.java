b6ba64f9f6d8d8c7abd4f6306dc9c56d52c50e07
oibanez1 <oibanezdaniel1@gmail.com>
2021-08-04 15:12:44 -0400

    implemented get/add/edit/delete user methods, added doPost method

--- src/main/java/com/example/servlet/UserServlet.java
+++ src/main/java/com/example/servlet/UserServlet.java
@@ -2,6 +2,11 @@ package com.example.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,22 +16,50 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet blah blah.
  */
 
-@WebServlet(description = "servlet one", urlPatterns = { "/myapp" })
+import com.example.dao.UserDAO;
+import com.example.model.User;
+
+import org.w3c.dom.UserDataHandler;
+
+@WebServlet(description = "servlet one", urlPatterns = { "/app" })
 public class UserServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private UserDAO userDAO;
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
+    
+     /**
+      * new obj to have for the db connection.
+      */
+     public void init(){
+         userDAO = new UserDAO();
+     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
         IOException {
-        response.setContentType("text/html");
-        String params = request.getParameter("userInput");
-        PrintWriter writer = response.getWriter();
-        if (params != null) {
-            writer.printf("borpa %s", params);
-        } else {
-            writer.print("borpaLookingAtYou");
+        String path = request.getServletPath();
+        try{
+            if(path.equals("/new")){
+                newForm(request, response);
+            }
+            else if(path.equals("/add")){
+                addUser(request, response);
+            }
+            else if(path.equals("/delete")){
+                deleteUser(request, response);
+            }
+            else if(path.equals("/edit")){
+                editForm(request, response);
+            }
+            else if(path.equals("/update")){
+                updateUser(request, response);
+            }
+            else {
+                allUsers(request, response);
+            }
+        }catch (ServletException ex){
+            System.out.println(ex);
         }
     }
 
@@ -36,9 +69,51 @@ public class UserServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
         IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        String userInput = request.getParameter("userInput");
-        out.println("borpaSPIN from POST YEP" + userInput);
+        doGet(request, response);
     }
+
+    private void newForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.getRequestDispatcher("/WEB-INF/form.jsp").forward(request, response);
+    }
+
+    private void editForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        int id = Integer.parseInt(request.getParameter("id"));
+        User selectedUser = userDAO.get(id);
+        request.setAttribute("selectedUser", selectedUser);
+        request.getRequestDispatcher("/WEB-INF/form.jsp").forward(request, response);
+    }
+
+    private void allUsers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        List<User> userList = userDAO.getAll();
+        request.setAttribute("userList", userList);
+        request.getRequestDispatcher("/WEB-INF/all_users.jsp").forward(request, response);
+    }
+
+    private void addUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        int age = Integer.parseInt(request.getParameter("age"));
+        User newUser = new User(name, email, country, age);
+        userDAO.add(newUser);
+        response.sendRedirect("list");
+    }
+    
+    private void updateUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name  = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        int age = Integer.parseInt(request.getParameter("age"));
+        User user = new User(id, name, email, country, age);
+        userDAO.update(user);
+        response.sendRedirect("list");
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDAO.delete(id);
+        response.sendRedirect("list");
+    }
+
 }
